d3bcfbe463f3bdbfd90864a18cb130dafb8acb3c
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-12-24 13:00:56 +0530

    push

---
+++ Week 2.2 | Express and Backend (Zoom upload)/http-server-1/index.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/route-handler", function (req, res) {
+  // headers, body, query parameter
+  //do machine learning model
+  res.json({ name: "SACHIN", age: 27 });
+});
+
+app.post("/backend-api/conversation", function (req, res) {
+  const message = req.body.message;
+  console.log("message", message);
+  res.json({
+    output: "2 + 2 = 4",
+  });
+});
+
+app.post("/", function (req, res) {
+  const body = req.body;
+  console.log("body", body);
+  res.send("Hello World!!");
+});
+
+app.get("/", function (req, res) {
+  res.json({ name: "SACHIN", age: 27 });
+});
+
+app.listen(port, () => {
+  console.log(`Example of app listening on port ${port}`);
+});
