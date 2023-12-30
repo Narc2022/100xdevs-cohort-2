6b0dd6bc2fe34afbcbf548fb56f2cafcf6dea840
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-12-30 18:54:39 +0530

    ZOD TO RESUME 01:21

--- Week 3.1 | Middlewares, global catches and zod/http-server-6better-input-validation(zod)/index.js
+++ Week 3.1 | Middlewares, global catches and zod/http-server-6better-input-validation(zod)/index.js
@@ -11,6 +11,12 @@ app.post("/health-checkup", function (req, res) {
   const kidneys = req.body.kidneys;
 
   const response = schema.safeParse(kidneys);
+  if (!response.success) {
+    res.status(411).json({
+      error: response.error.issues || "Input is Invalid",
+    });
+  }
+
   res.send({
     response,
   });
