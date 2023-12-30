2325521d9ea18e42a71258335e4cbd56e32db76c
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-12-30 18:21:55 +0530

    starting zod validation

--- Week 3.1 | Middlewares, global catches and zod/http-server-6better-input-validation(zod)/index.js
+++ Week 3.1 | Middlewares, global catches and zod/http-server-6better-input-validation(zod)/index.js
@@ -3,12 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-//Middleware
-function middleWare(req, res, next) {
-  fetch().then(() => {
-    next();
-  });
-}
 
 app.post("/health-checkup", function (req, res) {
   console.log("req.body.kidneys", req.body.kidneys);
@@ -18,8 +12,4 @@ app.post("/health-checkup", function (req, res) {
   res.send("you have " + kidneyLength + " kidneys");
 });
 
-//Global Catch
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500).json("sorry somthing is wrong with server");
-});
 app.listen(3000);
