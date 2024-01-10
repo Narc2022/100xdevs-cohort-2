2d69feab65585d575795d986914e55fa6817630d
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-01-10 09:18:15 +0530

    before starting mongodb integration learning

--- 3.2 | Databases and Authentication (Local Recording)/assignment-auth-jwt/assignment.js
+++ 3.2 | Databases and Authentication (Local Recording)/assignment-auth-jwt/assignment.js
@@ -1,72 +1,76 @@
-const express = require('express');
+const express = require("express");
 const jwt = require("jsonwebtoken");
 const jsonPassword = "123456";
 
-
 const app = express();
 app.use(express.json());
-//In memory database 
+//In memory database
 const ALL_USERS = [
-    {
-        username:"sac@gmail.com",
-        name:"sac singh",
-        password:"1234"
-    },
-    {
-        username:"sac1@gmail.com",
-        name:"sac1 singh",
-        password:"345678"
-    },
-    {
-        username:"sac22@gmail.com",
-        name:"sac22 singh",
-        password:"234567"
-    }
-]
+  {
+    username: "sac@gmail.com",
+    name: "sac singh",
+    password: "1234",
+  },
+  {
+    username: "sac1@gmail.com",
+    name: "sac1 singh",
+    password: "345678",
+  },
+  {
+    username: "sac22@gmail.com",
+    name: "sac22 singh",
+    password: "234567",
+  },
+];
 
-function userExists(username,password){
-    console.log('userExists username', username)
-    console.log('userExists password', password)
-    //write logic here
-    let userExists = false;
-    for (let i = 0; i < ALL_USERS.length; i++) {
-        if(ALL_USERS[i].username == username && ALL_USERS[i].password == password){
-            userExists = true;
-            console.log('userExists', userExists)
-        }
+function userExists(username, password) {
+  console.log("userExists username", username);
+  console.log("userExists password", password);
+  //write logic here
+  let userExists = false;
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (
+      ALL_USERS[i].username == username &&
+      ALL_USERS[i].password == password
+    ) {
+      userExists = true;
+      console.log("userExists", userExists);
     }
-    return userExists
-
+  }
+  return userExists;
 }
 
-app.post("/signin",function(req,res){
-     const username = req.body.username;
-     const password = req.body.password;
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-     if(!userExists(username,password)){
-      return res.status(403).json({
-        message:"user doesn't exist in our memory db"
-      })
-     }
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      message: "user doesn't exist in our memory db",
+    });
+  }
 
-     var token = jwt.sign({username:username},jsonPassword);
-     return res.json({
-        token,
-     })
-})
+  var token = jwt.sign({ username: username }, jsonPassword);
+  return res.json({
+    token,
+  });
+});
 // 1:15
-app.get("/users",function(req,res){
-    const token = req.headers.authorization;
-    try {
-        const decoded = jwt.verify(token,jsonPassword);
-        const username = decoded.username;
-        return res.json({...userExists,username});
-    } catch (error) {
-        return res.status(403).json({
-            msg:"Invalid Token"
-        })
-    }
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  console.log("token", token);
+  const decoded = jwt.verify(token, jsonPassword);
+  const username = decoded.username;
+  console.log("username", username);
+  return res.json({
+    users: ALL_USERS.filter(function(value){
+      if (value.username == username) {
+        return false;
+      } else {
+        return true;
+      }
+    }),
+  });
+});
 
-})
-
-app.listen(3001)
+app.listen(3001);
