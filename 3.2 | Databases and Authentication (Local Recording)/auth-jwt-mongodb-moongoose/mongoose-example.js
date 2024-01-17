d9a480f70ed86cb4442b5c894ff8c74f2e67f33d
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-01-17 21:05:08 +0530

    update

--- 3.2 | Databases and Authentication (Local Recording)/auth-jwt-mongodb-moongoose/mongoose-example.js
+++ 3.2 | Databases and Authentication (Local Recording)/auth-jwt-mongodb-moongoose/mongoose-example.js
@@ -1,10 +1,35 @@
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://100xdevssachin:sachin2024@100xdevs.uisiqme.mongodb.net/userappnew");
+
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://100xdevssachin:sachin2024@100xdevs.uisiqme.mongodb.net/userappnew"
+);
 
 //schema look like
-const User = mongoose.model('User',{name:String, email:String, password:String});
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+});
 
-const user = new User({name:'Sachin Singh', email:'100xdevssachin@gmail.com',password:'123457'});
+app.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
 
-user.save();
+  const existingUser = User.findOne({ email: username });
+  console.log('existingUser', existingUser)
+  //CRUD create,update,delete, read
+  if (existingUser) {
+    return res.status(400).send("Username already exists");
+  }
+  const user = new User({ name: name, email: "username", password: password });
+  user.save();
+  res.json({
+    msg: "Use created successfully",
+  });
+});
+app.listen(3000)
