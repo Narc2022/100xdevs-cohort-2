a5e7225a18042581e3ec8e66c3c846e354cb385a
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-02-05 09:12:06 +0530

    connect uri to moongose

--- assignments-local/paytm-main/backend/db.js
+++ assignments-local/paytm-main/backend/db.js
@@ -1,17 +1,40 @@
 // mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://100xdevssachin:sachin243@100xdevs.uisiqme.mongodb.net/paytm"
+);
 
 //user schema
 const userSchema = new mongoose.Schema({
-     username:String,
-     password:String,
-     firstName:String,
-     lastName:String
-})
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    require: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    require: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
 
 //create model from schema
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.export={
-    User
-}
+module.export = {
+  User,
+};
