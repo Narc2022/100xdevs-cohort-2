f1db48a67ef2baed6c48b3701b3c670c57e2f3be
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-02-08 12:50:33 +0530

    paytm changes

--- assignments-local/paytm-main/backend/index.js
+++ assignments-local/paytm-main/backend/index.js
@@ -1,2 +1,15 @@
 const express = require("express");
+const mainRouter = require("./routes/index");
 const app = express();
+
+app.use("/api/v1", mainRouter);
+
+// syntax app.use(path, callback)
+
+// /api/v1/user/signup
+// /api/v1/user/signin
+// /api/v1/user/changepass
+
+// /api/v1/account/transferMoney
+// /api/v1/account/balance
+//
