223187a41309862a21e7cb9a8003cec0a386afc8
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-02-15 20:53:14 +0530

    account route in progress

--- assignments-local/paytm-main/backend/routes/index.js
+++ assignments-local/paytm-main/backend/routes/index.js
@@ -1,9 +1,10 @@
-// backend/user/index.js
 const express = require("express");
 const userRouter = require("./user");
+const accountRouter = require("./account");
 
 const router = express.Router();
 
 router.use("/user", userRouter);
+router.use("/Account", accountRouter);
 
 module.exports = router;
